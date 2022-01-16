2f04d9059e2f750f4cc3bcbeda9467522f282c06
Talia-Apr <taliaaprianti46@gmail.com>
2022-01-16 21:02:54 +0700

    Materi Laravel dan Tailwind CSS
    Materi Laravel dan Tailwind CSS Vid 1-19

---
+++ laravel-resto/resources/views/admin/login.blade.php
@@ -0,0 +1,6 @@
+@extends('template.layout')
+@section('content')
+
+<h1 class="bg-red-200">Login Admin</h1>
+
+@endsection
---
+++ laravel-resto/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('login', function(){
+    return view ('admin.login');
+});
+
+Route::get('/', [HomeController::class,'index']);
+// Route::get('/store', [HomeController::class,'store']);
+Route::post('/store', [HomeController::class,'store']);
+Route::post('/store/{id}', [HomeController::class,'update']);
+Route::get('/store/{id}', [HomeController::class,'destroy']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
